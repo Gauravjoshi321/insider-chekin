c18279cede9282332fa5f13993b6ef85d69227fa
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-14 13:34:20 +0530

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: 200;
+`;
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  color: orange;
+  background-color: #00c8ff;
+  border: none;
+  cursor: pointer;
+`;
+
+const StyledApp = styled.div`
+  background-color: red;
+`;
+
 function App() {
   return (
-    <div>Hello World!</div>
+    <StyledApp>
+      <H1>Hello World!</H1>
+      <Button onClick={() => alert("this is alert")}>check in</Button>
+    </StyledApp>
   )
 }
 
