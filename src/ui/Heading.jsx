3adedcb50b2b07471240c6d67c26aa9eb6022002
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-15 18:46:35 +0530

    Virat kohli

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -6,7 +6,7 @@ const Heading = styled.h1`
 ${props => props.as === 'h1' && css`
     font-size: 30px;
     font - weight: 200;
-    color: ${props.color}
+    color: ${props.color};
 `}
 ${props => props.as === 'h2' && css`
     font-size: 25px;
