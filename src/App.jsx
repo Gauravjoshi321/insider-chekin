7138f4053dc943dfb0e58f715f9df3796acfa0de
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-15 13:31:04 +0530

    Row

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Button from "./ui/Button";
 import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
-  background-color: red;
+  /* background-color: red; */
 `;
 
 function App() {
@@ -12,10 +12,16 @@ function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <Heading as='h1' color="green">Hello World!</Heading>
+
+        <Heading as='h1' color="orange">Hello World!</Heading>
         <Heading as='h2' color="blue">Hello World!</Heading>
-        <Heading as='h3'>Hello World!</Heading>
+        <Heading as='h3' color="green">Hello World!</Heading>
+
         <Button onClick={() => alert("this is alert")}>check in</Button>
+        <Button onClick={() => alert("this is alert")}>check out</Button>
+
+        <input type="number" placeholder="type any number" />
+        <input type="text" placeholder="type any text" />
       </StyledApp>
     </>
   )
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -3,25 +3,21 @@ import styled, { css } from "styled-components";
 // const color = css`color: white`
 
 const Heading = styled.h1`
-${props =>
-    props.as === 'h1' && css`
+${props => props.as === 'h1' && css`
     font-size: 30px;
     font - weight: 200;
     color: ${props.color}
 `}
-${props =>
-    props.as === 'h2' && css`
+${props => props.as === 'h2' && css`
     font-size: 25px;
     font - weight: 150;
     color: ${props.color}
 `}
-${props =>
-    props.as === 'h3' && css`
+${props => props.as === 'h3' && css`
     font-size: 20px;
     font - weight: 100;
-    color: white
+    color: ${props.color}
 `}
-
 `;
 
 export default Heading;
