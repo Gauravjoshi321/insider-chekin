41053222ceed9296d220760ff32cca666f1c35f8
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-21 19:54:59 +0530

    all deleted

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -23,3 +23,11 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  return (
+    <div></div>
+  )
+}
+
+export default CabinTable
