1a645c31a742ed8e7fbbd4ae5d9c603a702392f1
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-22 13:49:37 +0530

    deleteCabin

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Dashboard() {
+
   return (
     <Row type="horizontal">
       <Heading as="h1">Dashboard</Heading>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -7,7 +7,22 @@ export async function getCabins() {
 
   if (error) {
     console.log(error);
-    throw new Error("Cabins could not loaded");
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+
+  const { data, error } = await supabase
+    .from('cabins')
+    .delete()
+    .eq('id', id)
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
