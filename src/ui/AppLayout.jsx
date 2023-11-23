7677e3854f3318bbef4fa9fea629fbd7866d5480
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-23 12:54:57 +0530

    new added something previous

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -15,13 +15,23 @@ const StyledAppLayout = styled.div`
   height: 100vh;
 `
 
+const Container = styled.div`
+  max-width: 100rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`
+
 function AppLayout() {
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   )
