338e571cf0eaa02ae308db9142243e3c647e3899
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-11-25 13:50:05 +0530

    This field is Required

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,6 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
-import Spinner from "../../ui/Spinner";
 
 const FormRow = styled.div`
   display: grid;
@@ -42,10 +41,10 @@ const Label = styled.label`
   font-weight: 500;
 `;
 
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+// const Error = styled.span`
+//   font-size: 1.4rem;
+//   color: var(--color-red-700);
+// `;
 
 
 function CreateCabinForm() {
@@ -75,31 +74,35 @@ function CreateCabinForm() {
     mutate(data);
   }
 
+  // function onError(error) {
+  //   console.log(error);
+  // }
+
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register("name")} />
+        <Input type="text" id="name" {...register("name", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register("maxCapacity")} />
+        <Input type="number" id="maxCapacity" {...register("maxCapacity", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register("regularPrice")} />
+        <Input type="number" id="regularPrice" {...register("regularPrice", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} {...register("discount")} />
+        <Input type="number" id="discount" defaultValue={0} {...register("discount", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" {...register("description")} />
+        <Textarea type="number" id="description" defaultValue="" {...register("description", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow>
