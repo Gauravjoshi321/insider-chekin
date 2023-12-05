621efcf3651581763586f88d5b59c7e2d98ba972
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-05 15:18:58 +0530

    load earlier

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,4 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import { cloneElement, createContext, useContext, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -74,12 +74,27 @@ function Open({ opens: opensWindowName, children }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useRef();
+
+  useEffect(function () {
+    function handleClick(e) {
+      console.log(ref.current);
+      if (ref.current && !ref.current.contains(e.target)) {
+        console.log("click outside");
+        close();
+      }
+    }
+
+    document.addEventListener("click", handleClick, true);
+
+    return () => document.removeEventListener("click", handleClick, true);
+  })
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
