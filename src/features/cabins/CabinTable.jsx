61443be8d65bbcea644e8142b9f31ac4b66e8e7a
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-06 14:02:18 +0530

    Table.Body

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -50,7 +50,10 @@ function CabinTable() {
         <div></div>
       </Table.Header>
 
-      {cabins.map(cabin => <CabinRow cabin={cabin} key={cabin.id} />)}
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
 
     </Table>
   )
