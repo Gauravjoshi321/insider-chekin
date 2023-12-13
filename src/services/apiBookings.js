1bea85868b3ffbacd38f9121d9a346db9e79a5ea
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-13 18:57:38 +0530

    apiBookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,20 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+
+export async function getBookings() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
