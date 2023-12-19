38cf7e3f834ae7b33dce8c9e47119082f97a8383
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-19 20:05:50 +0530

    checked out jonas

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -9,6 +9,7 @@ import { formatDistanceFromNow } from "../../utils/helpers";
 import Menus from "../../ui/Menus";
 import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import useCheckout from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -53,6 +54,7 @@ function BookingRow({
 }) {
 
   const navigate = useNavigate();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -101,7 +103,10 @@ function BookingRow({
             </Menus.Button>
           }
           {status === "checked-in" &&
-            <Menus.Button onClick={() => { }}>
+            <Menus.Button
+              onClick={() => { checkout(bookingId) }}
+              disabled={isCheckingOut}
+            >
               <HiArrowUpOnSquare /> Check out
             </Menus.Button>
           }
--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateBooking } from "../../services/apiBookings";
 import toast from "react-hot-toast";
 
-function useCheckin() {
+function useCheckout() {
   const queryClient = useQueryClient();
 
   const {
@@ -27,4 +27,4 @@ function useCheckin() {
   return { checkout, isCheckingOut }
 }
 
-export default useCheckin;
+export default useCheckout;
