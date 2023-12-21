397aa55739a8b1a863b9855e6de3dafaa66a9112
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-21 19:48:58 +0530

    new Password

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       console.log(user);
-      navigate("/dashboard");
+      navigate("/");
     },
     onError: (err) => {
       console.log("ERROR", err);
