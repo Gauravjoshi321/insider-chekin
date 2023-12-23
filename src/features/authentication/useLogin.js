682ffebc10efde1222784372262af474ae09f41e
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-23 16:01:21 +0530

    places authorization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuthentication";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading: isLoggingIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
-      navigate("/");
+      queryClient.setQueryData(['user'], user);
+      navigate("/dashboard");
     },
     onError: (err) => {
       console.log("ERROR", err);
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,14 +1,40 @@
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import styled from "styled-components";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 function ProtectedRoute({ children }) {
 
+  const navigate = useNavigate();
+
   // 1. Check for the user if it is Authenticated by checking the session created for the current client
+  const { isLoading, isAuthenticated } = useUser();
 
-  // 2. Load the Spinner while isLoading
+  // 2. If not Authenticated then navigate the user to the login page
+  useEffect(function () {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate])
 
-  // 3. If not Authenticated then navigate the user to the login page
+  // 3. Load the Spinner while isLoading
+  if (isLoading) return (
+    <FullPage>
+      <Spinner />
+    </FullPage>
+  )
 
-  // 4. If Authenticated then return the children
 
-  return children
+  // // 4. If Authenticated then return the children
+  // if (isAuthenticated)
+  return children;
 }
 
 export default ProtectedRoute;
