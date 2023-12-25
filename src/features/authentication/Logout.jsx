f9581043a1de3efbaf2d356eecdf041cc2faeb51
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-25 13:01:21 +0530

    Logout.jsx

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,12 @@
+import ButtonIcon from "../../ui/ButtonIcon";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+
+function Logout() {
+  return (
+    <ButtonIcon>
+      <HiArrowRightOnRectangle />
+    </ButtonIcon>
+  )
+}
+
+export default Logout;
