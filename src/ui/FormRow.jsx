9453182c4a92639a32f90966424170b4db3786e3
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-25 19:32:40 +0530

    FormRow

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const FormRow = styled.div`
+const StyledFormRow = styled.div`
   display: grid;
   align-items: center;
   grid-template-columns: 24rem 1fr 1.2fr;
@@ -26,5 +26,24 @@ const FormRow = styled.div`
     gap: 1.2rem;
   }
 `;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+function FormRow({ label, error, children }) {
+  return (
+    <StyledFormRow>
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
+}
 
 export default FormRow;
