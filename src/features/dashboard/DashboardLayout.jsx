bc53ac71a640e08a2c92be11cffc6cc2340927f1
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-12-29 20:16:07 +0530

    SalesChart added

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -31,7 +31,7 @@ function DashboardLayout() {
       />
       <div>Today's activity</div>
       <div>Chart stay duration</div>
-      <SalesChart />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
 }
