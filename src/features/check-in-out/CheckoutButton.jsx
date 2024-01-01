346d283c512e00cabfeef51e876e64a8464909c9
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-01-01 15:06:42 +0530

    todaysActivity completed

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,16 @@
 import Button from "../../ui/Button";
+import useCheckout from "../../features/check-in-out/useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
