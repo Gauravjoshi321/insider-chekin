88f0a81f82fc3d312167513c52b6db6b5da54fa5
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-04-05 17:10:01 +0530

    issue with Row comp

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,20 @@
+import styled, { css } from "styled-components";
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+// import Row from "../ui/Row";
+
+const Row = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+`;
 
 function Dashboard() {
 
   return (
     <>
-      <Row type="horizontal">
+      <Row>
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
       </Row>
